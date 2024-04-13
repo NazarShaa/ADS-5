94bfbe9c8f67492c2ec2946ac23d8392530f812c
NazarShaa <160277049+NazarShaa@users.noreply.github.com>
2024-04-13 12:14:02 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -1,10 +1,47 @@
 // Copyright 2021 NNTU-CS
 #ifndef INCLUDE_TSTACK_H_
 #define INCLUDE_TSTACK_H_
+#include string
 
 template<typename T, int size>
 class TStack {
-  // добавьте код стека
+public:
+  TStack(int s) {
+    size = s;
+    data = new T[size];
+  }
+  void Push(const T& item) {
+    if (top <= size - 1)
+      data[top++] = item;
+    else
+      resize(2*size);
+  }
+  ~TStack(T* arr, int size) {
+    for (int i = 0; i < size; i++) {
+      delete arr[i];
+    }
+  }
+  bool isEmpty() const {
+    return top == 0;
+  }
+  T get() const {
+    if (!isEmpty())
+      throw std::string("stack is empty");
+    else 
+      return data[top];
+  }
+private:
+  T* data;
+  int top, size;
+  void resize(int nsize) {
+    T* temp = new T[nsize];
+    for (int i = 0; i < size; i++) {
+      temp[i] = data[i];
+    }
+    delete[] data;
+    data = temp;
+    size = nsize;
+  }
 };
 
 #endif  // INCLUDE_TSTACK_H_
