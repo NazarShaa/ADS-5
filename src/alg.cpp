6b251a96166e11d170c94efdb58a723abe1d5eed
NazarShaa <160277049+NazarShaa@users.noreply.github.com>
2024-04-14 23:40:35 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -2,7 +2,7 @@
 #include <map>
 #include "tstack.h"
 
-int Priority (char c) {
+int Priority(char c) {
   if (c == '(')
     return 0;
   else if (c == ')')
@@ -17,32 +17,31 @@ int Priority (char c) {
 std::string infx2pstfx(std::string inf) {
   std::string post;
   TStack<char, 100> stack1;
-  for (char c: inf) {
-    if (Priority(c) == -1)
-      post += c
+  for (char c : inf) {
+    if (Priority(c) == -1) {
+      post += c;
       post += ' ';
-    else {
-      if (c == ')') {
-        while (stack1.get() != '(') {
-	  post += stack1.pop();
-	  post += ' ';
-	}
-	stack1.pop();
-      }
-      else if (c == '(' || stack1.isEmpty())
-        stack1.push(c);
-      else if (!stack1.isEmpty()) {
-	char elem = stack1.get();
-	if (Priority(elem) < Priority(c))
-	  stack1.push(c);
-	else {
-	  while (Priority(elem) >= Priority(c) && !stack1.isEmpty()) {
-	    post += stack1.pop();
-	    post += ' ';
-	  }
-	  stack1.push(c);
-	}
-      }
+    } else {
+        if (c == ')') {
+          while (stack1.get() != '(') {
+            post += stack1.pop();
+            post += ' ';
+          }
+          stack1.pop();
+      } else if (c == '(' || stack1.isEmpty())
+          stack1.push(c);
+        else if (!stack1.isEmpty()) {
+	  char elem = stack1.get();
+          if (Priority(elem) < Priority(c))
+            stack1.push(c);
+          else {
+            while (Priority(elem) >= Priority(c) && !stack1.isEmpty()) {
+              post += stack1.pop();
+              post += ' ';
+            }
+            stack1.push(c);
+          }
+        }
     }
   }
   while (!stack1.isEmpty()) {
@@ -62,32 +61,30 @@ int eval(std::string post) {
       else if (isdigit(post[i + 1])) {
         number += post[i];
 	continue;
+      } else {
+	  number += post[i];
+	  int value = atoi(number.c_str());
+          stack2.push(value);
+          number = "";
+        }
+    } else {
+        int b = stack2.pop();
+        int a = stack2.pop();
+        switch (post[i]) {
+        case '+':
+          stack2.push(a + b);
+	  break;
+        case '-':
+	  stack2.push(a - b);
+          break;
+        case '*':
+          stack2.push(a * b);
+          break;
+        case '/':
+          stack2.push(a / b);
+          break;
+        }
       }
-      else {
-	number += post[i];
-	int value = atoi(number.c_str());
-	stack2.push(value);
-	number = "";
-      }
-    }
-    else {
-      int b = stack2.pop();
-      int a = stack2.pop();
-      switch (post[i]) {
-      case '+':
-        stack2.push(a + b);
-	break;
-      case '-':
-	stack2.push(a - b);
-	break;
-      case '*':
-	stack2.push(a * b);
-	break;
-      case '/':
-	stack2.push(a / b);
-        break;
-      }
-    }
   }
   return stack2.get();
 }
