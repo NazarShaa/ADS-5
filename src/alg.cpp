1fe4166bd43e118c3f2bef516e75a3842d93c1c5
NazarShaa <160277049+NazarShaa@users.noreply.github.com>
2024-04-14 21:12:38 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,59 +1,98 @@
 // Copyright 2021 NNTU-CS
-#include <string>
 #include <map>
 #include "tstack.h"
 
 int Priority (char c) {
   if (c == '(')
     return 0;
-  if (c == ')')
+  else if (c == ')')
     return 1;
-  if (c == '+' || c == '-')
+  else if (c == '+' || c == '-')
     return 2;
-  if (c == '*' || c == '/')
+  else if (c == '*' || c == '/')
     return 3;
+  else
+    return -1;
 }
 std::string infx2pstfx(std::string inf) {
-  std::string post
-   TStack<char, 100> stack1;
-  for (char c: inf){
-    if (c > '0' && c <= '9') {
-      post += c;
-      post += ' ';
-    }
+  std::string post;
+  TStack<char, 100> stack1;
+  for (char c: inf) {
+    if (c > '0' && c <= '9')
+      post += c + ' ';
     else {
-      if (c == '(')
-        stack1.Push(c);
-      if (stack1.get == 0)
-        stack1.Push(c);
-      if (stack1.get != 0 && Priority(stack1.get) < Priority(c))
-        stack1.Push(c);
-      if (c == ')') {
-        while (stack1.get != '(') {
-          post += stack1.pop;
-          post += ' ';
-        }
-        stack1.pop;
-      }
-      if (stack1.get != 0 && Priority(stack1.get) >= Priority(c)) {
-        while (stack1.get != 0 && Priority(stack1.get) <= Priority(c)) {
-          post += stack1.pop;
-          post += ' ';
-        }
-        stack1.Push(c);
-      }
-      if (c == '/0') {
-        while (stack.get != 0) {
-          post += stack.pop;
-          post += ' ';
-        }
-      }
-    } 
-  }
-  return post;
+      char sym = stack1.get();
+			if (c == '(' || Priority(sym) < Priority(c) || stack1.isEmpty() || c == ')') {
+				while (Priority(sym) >= Priority(c)) {
+					post += sym + ' ';
+					stack1.pop();
+					if (!stack1.isEmpty())
+						sym = stack1.get();
+					else
+						break;
+				}
+				if (c == ')')
+					stack1.pop();
+				else
+					stack1.push(c);
+			}
+			else {
+				while (Priority(sym) >= Priority(c)) {
+					post += sym + ' ';
+					stack1.pop();
+					if (!stack1.isEmpty())
+						sym = stack1.get();
+					else
+						break;
+				}
+				stack1.push(c);
+			}
+		}
+	}
+	while (!stack1.isEmpty()) {
+		post += stack1.get() + ' ';
+		stack1.pop();
+	}
+	return post;
 }
 
 int eval(std::string post) {
-  
-  return 0;
+	TStack<int, 100> stack2;
+	std::string number;
+	for (int i = 0; i < post.size(); ++i) {
+		if (Priority(post[i]) == -1) {
+			if (post[i] == ' ')
+				continue;
+			else if (isdigit(post[i + 1])) {
+				number += post[i];
+				continue;
+			}
+			else {
+				number += post[i];
+				int value = atoi(number.c_str());
+				stack2.push(value);
+				number = "";
+			}
+		}
+		else {
+			int b = stack2.pop();
+			int a = stack2.pop();
+			switch (post[i]) {
+			case '+':
+				stack2.push(a + b);
+				break;
+			case '-':
+				stack2.push(a - b);
+				break;
+			case '*':
+				stack2.push(a * b);
+				break;
+			case '/':
+				stack2.push(a / b);
+				break;
+			}
+		}
+
+	}
+	return stack2.get();
 }
