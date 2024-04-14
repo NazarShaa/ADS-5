bbd8efe71198c181cf423e53c936d7b629b366a7
NazarShaa <160277049+NazarShaa@users.noreply.github.com>
2024-04-14 21:09:08 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -7,46 +7,44 @@
 template<typename T, int size>
 class TStack {
 public:
-	TStack() : top(-1) {
-		data = new T[size];
-	}
-	void push(const T& item) {
-		if (top <= size - 1)
-			data[++top] = item;
-		else
-			resize(2 * size);
-	}
-	T pop() {
-		if (!isEmpty())
-			throw std::string("stack is empty");
-		else
-			return data[top--];
-	}
-	~TStack() {
-		delete[] data;
-
-	}
-	bool isEmpty() const {
-		return top == -1;
-	}
-	T get() const {
-		if (!isEmpty())
-			throw std::string("stack is empty");
-		else
-			return data[top];
-	}
+  TStack() : top(-1) {
+    data = new T[size];
+  }
+  void push(const T& item) {
+    if (top <= size - 1)
+      data[++top] = item;
+    else
+      resize(2 * size);
+  }
+  T pop() {
+    if (!isEmpty())
+      throw std::string("stack is empty");
+    else
+      return data[top--];
+  }
+  ~TStack() {
+    delete[] data;
+  }
+  bool isEmpty() const {
+    return top == -1;
+  }
+  T get() const {
+    if (!isEmpty())
+      throw std::string("stack is empty");
+    else
+      return data[top];
+  }
 private:
-	T* data;
-	int top, size;
-	void resize(int nsize) {
-		T* temp = new T[nsize];
-		for (int i = 0; i < size; i++) {
-			temp[i] = data[i];
-		}
-		delete[] data;
-		data = temp;
-		size = nsize;
-	}
+  T* data;
+  int top, size;
+  void resize(int nsize) {
+    T* temp = new T[nsize];
+    for (int i = 0; i < size; i++) {
+      temp[i] = data[i];
+    }
+    delete[] data;
+    data = temp;
+    size = nsize;
+  }
 };
-
 #endif  // INCLUDE_TSTACK_H_
